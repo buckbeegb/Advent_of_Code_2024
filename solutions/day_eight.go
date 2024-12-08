444f54a16a29f0e2ca6ead5aab55d3468bf15c94
Brendan Buckbee <buckbeegb@gmail.com>
2024-12-08 12:05:30 -0600

    Terrible looking but functional day 8 solution

--- solutions/day_eight.go
+++ solutions/day_eight.go
@@ -2,11 +2,17 @@ package solutions
 
 import (
 	"bufio"
-	// "fmt"
+	"fmt"
 	"os"
 	// "strconv"
 	"strings"
 )
+
+type Antenna_pair struct {
+	signal        rune
+	row_positions []int
+	col_positions []int
+}
 
 func Day_eight_part_one() {
 	dat, err := os.ReadFile("./Full_Inputs/day_eight.txt")
@@ -15,9 +21,42 @@ func Day_eight_part_one() {
 		panic(err)
 	}
 	scanner := bufio.NewScanner(strings.NewReader(string(dat)))
+	line_num := 0
+	grid_width := 0
+	antenna_map := make(map[rune]Antenna_pair)
 	for scanner.Scan() {
+		for i, char := range scanner.Text() {
+			if char != '.' {
+				if antenna_map[char].signal == 0 {
+					antenna_map[char] = Antenna_pair{signal: char, row_positions: []int{line_num}, col_positions: []int{i}}
+				} else {
+					antenna_set := antenna_map[char]
+					antenna_set.row_positions = append(antenna_set.row_positions, line_num)
+					antenna_set.col_positions = append(antenna_set.col_positions, i)
+					antenna_map[char] = antenna_set
+				}
+			}
+			grid_width = i + 1
+		}
+		line_num++
 	}
+	antinode_map := make(map[Point]bool)
+	for key := range antenna_map {
+		antenna := antenna_map[key]
+		for i := 0; i < len(antenna.row_positions); i++ {
+			for j := i + 1; j < len(antenna.row_positions); j++ {
+				antinodes := determine_antinodes(Point{row: antenna.row_positions[i], col: antenna.col_positions[i]}, Point{row: antenna.row_positions[j], col: antenna.col_positions[j]}, line_num, grid_width)
+				for _, antinode := range antinodes {
+					antinode_map[antinode] = true
+				}
+			}
+		}
+	}
+	unique_antinodes := len(antinode_map)
+	// print_grid(antinode_map, antenna_map, line_num, grid_width)
+	fmt.Println(unique_antinodes)
 }
+
 func Day_eight_part_two() {
 	dat, err := os.ReadFile("./Full_Inputs/day_eight.txt")
 	// dat, err := os.ReadFile("./Test_Inputs/day_eight.txt")
@@ -25,6 +64,139 @@ func Day_eight_part_two() {
 		panic(err)
 	}
 	scanner := bufio.NewScanner(strings.NewReader(string(dat)))
+	line_num := 0
+	grid_width := 0
+	antenna_map := make(map[rune]Antenna_pair)
 	for scanner.Scan() {
+		for i, char := range scanner.Text() {
+			if char != '.' {
+				if antenna_map[char].signal == 0 {
+					antenna_map[char] = Antenna_pair{signal: char, row_positions: []int{line_num}, col_positions: []int{i}}
+				} else {
+					antenna_set := antenna_map[char]
+					antenna_set.row_positions = append(antenna_set.row_positions, line_num)
+					antenna_set.col_positions = append(antenna_set.col_positions, i)
+					antenna_map[char] = antenna_set
+				}
+			}
+			grid_width = i + 1
+		}
+		line_num++
+	}
+	antinode_map := make(map[Point]bool)
+	for key := range antenna_map {
+		antenna := antenna_map[key]
+		for i := 0; i < len(antenna.row_positions); i++ {
+			for j := i + 1; j < len(antenna.row_positions); j++ {
+				antinodes := determine_part_2_antinodes(Point{row: antenna.row_positions[i], col: antenna.col_positions[i]}, Point{row: antenna.row_positions[j], col: antenna.col_positions[j]}, line_num, grid_width)
+				for _, antinode := range antinodes {
+					antinode_map[antinode] = true
+				}
+			}
+		}
+	}
+	unique_antinodes := len(antinode_map)
+	// fmt.Println()
+	// print_grid(antinode_map, antenna_map, line_num, grid_width)
+	fmt.Println(unique_antinodes)
+}
+
+func determine_antinodes(a Point, b Point, grid_height int, grid_width int) []Point {
+	antinodes := []Point{}
+	row_corr := float64(a.row-b.row) / float64(a.col-b.col)
+	big_row, small_row := sort_ints(a.row, b.row)
+	big_col, small_col := sort_ints(a.col, b.col)
+	row_distance := big_row - small_row
+	col_distance := big_col - small_col
+	var antinode_1 Point
+	var antinode_2 Point
+	if row_corr > 0 {
+		antinode_1 = Point{row: big_row + row_distance, col: big_col + col_distance}
+		antinode_2 = Point{row: small_row - row_distance, col: small_col - col_distance}
+	} else {
+		antinode_1 = Point{row: big_row + row_distance, col: small_col - col_distance}
+		antinode_2 = Point{row: small_row - row_distance, col: big_col + col_distance}
+	}
+	if antinode_1.row >= 0 && antinode_1.row < grid_height && antinode_1.col >= 0 && antinode_1.col < grid_width {
+		antinodes = append(antinodes, antinode_1)
+	}
+	if antinode_2.row >= 0 && antinode_2.row < grid_height && antinode_2.col >= 0 && antinode_2.col < grid_width {
+		antinodes = append(antinodes, antinode_2)
+	}
+	return antinodes
+}
+
+func determine_part_2_antinodes(a Point, b Point, grid_height int, grid_width int) []Point {
+	antinodes := []Point{}
+	row_corr := float64(a.row-b.row) / float64(a.col-b.col)
+	big_row, small_row := sort_ints(a.row, b.row)
+	big_col, small_col := sort_ints(a.col, b.col)
+	row_distance := big_row - small_row
+	col_distance := big_col - small_col
+	var antinode_1 Point
+	var antinode_2 Point
+	for i := 0; i < grid_height; i++ {
+		antinode_1, antinode_2 = update_positions(big_row, big_col, small_row, small_col, row_corr, row_distance, col_distance, i)
+		count_out_of_bounds := 0
+		if antinode_1.row >= 0 && antinode_1.row < grid_height && antinode_1.col >= 0 && antinode_1.col < grid_width {
+			antinodes = append(antinodes, antinode_1)
+		} else {
+			count_out_of_bounds++
+		}
+		if antinode_2.row >= 0 && antinode_2.row < grid_height && antinode_2.col >= 0 && antinode_2.col < grid_width {
+			antinodes = append(antinodes, antinode_2)
+		} else {
+			count_out_of_bounds++
+		}
+		if count_out_of_bounds == 2 {
+			break
+		}
+	}
+	return antinodes
+}
+
+func update_positions(big_row int, big_col int, small_row int, small_col int, row_corr float64, row_distance int, col_distance int, i int) (Point, Point) {
+	if row_corr > 0 {
+		return Point{row: big_row + (i * row_distance), col: big_col + (i * col_distance)}, Point{row: small_row - (i * row_distance), col: small_col - (i * col_distance)}
+	}
+	return Point{row: big_row + (i * row_distance), col: small_col - (i * col_distance)}, Point{row: small_row - (i * row_distance), col: big_col + (i * col_distance)}
+}
+
+func sort_ints(a int, b int) (int, int) {
+	if a > b {
+		return a, b
+	}
+	return b, a
+}
+
+func abs_val(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func print_grid(antinode_map map[Point]bool, antenna_set map[rune]Antenna_pair, grid_height int, grid_width int) {
+	for i := 0; i < grid_height; i++ {
+		for j := 0; j < grid_width; j++ {
+			printed_something := false
+			for key := range antenna_set {
+				antenna := antenna_set[key]
+				for k := 0; k < len(antenna.row_positions); k++ {
+					if antenna.row_positions[k] == i && antenna.col_positions[k] == j {
+						fmt.Print(string(antenna.signal))
+						printed_something = true
+					}
+				}
+			}
+			if !printed_something && antinode_map[Point{row: i, col: j}] {
+				fmt.Print("X")
+				printed_something = true
+			}
+			if !printed_something {
+				fmt.Print(".")
+			}
+		}
+		fmt.Println()
 	}
 }
--- solutions/day_six.go
+++ solutions/day_six.go
@@ -60,8 +60,8 @@ func Day_six_part_one() {
 }
 
 func Day_six_part_two() {
-	// dat, err := os.ReadFile("./Full_Inputs/day_six.txt")
-	dat, err := os.ReadFile("./Test_Inputs/day_six.txt")
+	dat, err := os.ReadFile("./Full_Inputs/day_six.txt")
+	// dat, err := os.ReadFile("./Test_Inputs/day_six.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -77,75 +77,56 @@ func Day_six_part_two() {
 			}
 		}
 	}
-	walked_on := make(map[Point]bool)
+	walked_on := make(map[Point][]Point)
 	current_point := starting_point
 	current_direction := Point{-1, 0}
-	total_walked_on := 0
 	for current_point.row >= 0 && current_point.row < len(walking_map) && current_point.col >= 0 && current_point.col < len(walking_map[current_point.row]) {
-		if !walked_on[current_point] {
-			total_walked_on++
-			walked_on[current_point] = true
+		if walked_on[current_point] == nil {
+			walked_on[current_point] = []Point{current_direction}
 		}
 		if current_point.row+current_direction.row < 0 || current_point.row+current_direction.row >= len(walking_map) || current_point.col+current_direction.col < 0 || current_point.col+current_direction.col >= len(walking_map[current_point.row]) {
 			break
 		}
 		if walking_map[current_point.row+current_direction.row][current_point.col+current_direction.col] == '#' {
-			if current_direction.col == 0 && current_direction.row == -1 {
-				current_direction = Point{0, 1}
-			} else if current_direction.col == 1 && current_direction.row == 0 {
-				current_direction = Point{1, 0}
-			} else if current_direction.col == 0 && current_direction.row == 1 {
-				current_direction = Point{0, -1}
-			} else if current_direction.col == -1 && current_direction.row == 0 {
-				current_direction = Point{-1, 0}
-			}
+			current_direction = Point{current_direction.col, -current_direction.row}
 		}
 		current_point = Point{current_point.row + current_direction.row, current_point.col + current_direction.col}
 	}
 	total_valid_obstacles := 0
-	for i := 0; i < len(walking_map); i++ {
-		for j := 0; j < len(walking_map[i]); j++ {
-			if walking_map[i][j] == '#' || walking_map[i][j] == '^' || walked_on[Point{i, j}] == false {
-				continue
+	for key := range walked_on {
+		walking_map[key.row][key.col] = '#'
+		initial_direction := walked_on[key][0]
+		current_point := Point{key.row - initial_direction.row, key.col - initial_direction.col}
+		current_direction := initial_direction
+		infinite_loop := false
+		walked_on := make(map[Point][]Point)
+		for current_point.row >= 0 && current_point.row < len(walking_map) && current_point.col >= 0 && current_point.col < len(walking_map[current_point.row]) && !infinite_loop {
+			if walked_on[current_point] == nil {
+				walked_on[current_point] = []Point{}
 			} else {
-				walking_map[i][j] = '#'
-			}
-			current_point := starting_point
-			current_direction := Point{-1, 0}
-			infinite_loop := false
-			walked_on := make(map[Point][]Point)
-			for current_point.row >= 0 && current_point.row < len(walking_map) && current_point.col >= 0 && current_point.col < len(walking_map[current_point.row]) && !infinite_loop {
-				if current_point.row+current_direction.row < 0 || current_point.row+current_direction.row >= len(walking_map) || current_point.col+current_direction.col < 0 || current_point.col+current_direction.col >= len(walking_map[current_point.row]) {
-					break
-				}
-				for walking_map[current_point.row+current_direction.row][current_point.col+current_direction.col] == '#' {
-					if current_direction.col == 0 && current_direction.row == -1 {
-						current_direction = Point{0, 1}
-					} else if current_direction.col == 1 && current_direction.row == 0 {
-						current_direction = Point{1, 0}
-					} else if current_direction.col == 0 && current_direction.row == 1 {
-						current_direction = Point{0, -1}
-					} else if current_direction.col == -1 && current_direction.row == 0 {
-						current_direction = Point{-1, 0}
+				for _, val := range walked_on[current_point] {
+					if current_direction == val {
+						infinite_loop = true
+						break
 					}
 				}
-				if walked_on[current_point] == nil {
-					walked_on[current_point] = []Point{}
-				} else {
-					for _, val := range walked_on[current_point] {
-						if current_direction == val {
-							infinite_loop = true
-							break
-						}
-					}
-				}
-				walked_on[current_point] = append(walked_on[current_point], current_direction)
-				current_point = Point{current_point.row + current_direction.row, current_point.col + current_direction.col}
 			}
-			walking_map[i][j] = '.'
-			if infinite_loop {
-				total_valid_obstacles++
+			walked_on[current_point] = append(walked_on[current_point], current_direction)
+			next_row := current_point.row + current_direction.row
+			next_col := current_point.col + current_direction.col
+			if next_row < 0 || next_row >= len(walking_map) || next_col < 0 || next_col >= len(walking_map[current_point.row]) {
+				break
 			}
+			for walking_map[next_row][next_col] == '#' {
+				current_direction = Point{current_direction.col, -current_direction.row}
+				next_row = current_point.row + current_direction.row
+				next_col = current_point.col + current_direction.col
+			}
+			current_point = Point{current_point.row + current_direction.row, current_point.col + current_direction.col}
+		}
+		walking_map[key.row][key.col] = '.'
+		if infinite_loop {
+			total_valid_obstacles++
 		}
 	}
 	fmt.Println(total_valid_obstacles)
