80a20d83d41cf7897def4085b9da8a827b5df090
Brendan Buckbee <buckbeegb@gmail.com>
2024-12-18 14:21:45 -0600

    Completed Day 18 Solution :)

--- solutions/day_eighteen.go
+++ solutions/day_eighteen.go
@@ -2,29 +2,165 @@ package solutions
 
 import (
 	"bufio"
-	// "fmt"
+	"fmt"
 	"os"
-	// "strconv"
+	"strconv"
 	"strings"
 )
 
+type BFS_w_History struct {
+	Tail    Point
+	History map[Point]bool
+}
+
 func Day_eighteen_part_one() {
 	dat, err := os.ReadFile("./Full_Inputs/day_eighteen.txt")
+	max_size := 71
+	limit := 1024
 	// dat, err := os.ReadFile("./Test_Inputs/day_eighteen.txt")
+	// max_size := 7
+	// limit := 12
 	if err != nil {
 		panic(err)
 	}
-	scanner := bufio.NewScanner(strings.NewReader(string(dat)))
-	for scanner.Scan() {
+	grid := [][]string{}
+	for i := 0; i < max_size; i++ {
+		grid = append(grid, []string{})
+		for j := 0; j < max_size; j++ {
+			grid[i] = append(grid[i], ".")
+		}
 	}
+	scanner := bufio.NewScanner(strings.NewReader(string(dat)))
+	counter := 0
+	for scanner.Scan() && counter < limit {
+		int_split := strings.Split(scanner.Text(), ",")
+		col, _ := strconv.Atoi(int_split[0])
+		row, _ := strconv.Atoi(int_split[1])
+		grid[row][col] = "#"
+		counter++
+	}
+	position := Point{0, 0}
+	active_positions := []Point{position}
+	end_position := Point{max_size - 1, max_size - 1}
+	end_not_found := true
+	steps := 0
+	for end_not_found {
+		new_positions := []Point{}
+		for _, pos := range active_positions {
+			for _, dir := range []Point{{0, 1}, {1, 0}, {0, -1}, {-1, 0}} {
+				new_pos := Point{pos.row + dir.row, pos.col + dir.col}
+				if new_pos.row < 0 || new_pos.row >= max_size || new_pos.col < 0 || new_pos.col >= max_size {
+					continue
+				} else if grid[new_pos.row][new_pos.col] == "#" || grid[new_pos.row][new_pos.col] == "O" {
+					continue
+				} else if new_pos == end_position {
+					end_not_found = false
+					continue
+				} else {
+					new_positions = append(new_positions, new_pos)
+					grid[new_pos.row][new_pos.col] = "O"
+				}
+			}
+		}
+		active_positions = new_positions
+		steps++
+	}
+	fmt.Println(steps)
 }
+
 func Day_eighteen_part_two() {
 	dat, err := os.ReadFile("./Full_Inputs/day_eighteen.txt")
+	max_size := 71
+	limit := 1024
 	// dat, err := os.ReadFile("./Test_Inputs/day_eighteen.txt")
+	// max_size := 7
+	// limit := 12
 	if err != nil {
 		panic(err)
 	}
-	scanner := bufio.NewScanner(strings.NewReader(string(dat)))
-	for scanner.Scan() {
+	grid := [][]string{}
+	for i := 0; i < max_size; i++ {
+		grid = append(grid, []string{})
+		for j := 0; j < max_size; j++ {
+			grid[i] = append(grid[i], ".")
+		}
 	}
+	scanner := bufio.NewScanner(strings.NewReader(string(dat)))
+	obstacles := []Point{}
+	counter := 0
+	for scanner.Scan() {
+		int_split := strings.Split(scanner.Text(), ",")
+		col, _ := strconv.Atoi(int_split[0])
+		row, _ := strconv.Atoi(int_split[1])
+		if counter < limit {
+			grid[row][col] = "#"
+		}
+		obstacles = append(obstacles, Point{row, col})
+		counter++
+	}
+	i := limit + (len(obstacles)-limit)/2
+	max_range := len(obstacles) - 1
+	min_range := limit
+	for max_range-min_range > 1 {
+		position := Point{0, 0}
+		new_grid := clone_grid(grid)
+		for j := 0; j < i; j++ {
+			new_grid[obstacles[j].row][obstacles[j].col] = "#"
+		}
+		active_positions := []Point{position}
+		end_position := Point{max_size - 1, max_size - 1}
+		end_not_found := true
+		for end_not_found && len(active_positions) > 0 {
+			new_positions := []Point{}
+			for _, pos := range active_positions {
+				for _, dir := range []Point{{0, 1}, {1, 0}, {0, -1}, {-1, 0}} {
+					new_pos := Point{pos.row + dir.row, pos.col + dir.col}
+					if new_pos.row < 0 || new_pos.row >= max_size || new_pos.col < 0 || new_pos.col >= max_size {
+						continue
+					} else if new_grid[new_pos.row][new_pos.col] == "#" || new_grid[new_pos.row][new_pos.col] == "O" {
+						continue
+					} else if new_pos == end_position {
+						end_not_found = false
+						continue
+					} else {
+						new_positions = append(new_positions, new_pos)
+						new_grid[new_pos.row][new_pos.col] = "O"
+					}
+				}
+			}
+			active_positions = new_positions
+		}
+		if end_not_found {
+			max_range = i
+			i = min_range + (max_range-min_range)/2
+		} else {
+			min_range = i
+			i = min_range + (max_range-min_range)/2
+		}
+	}
+	fmt.Println(i, obstacles[i].col, obstacles[i].row)
+}
+
+func print_path(maze [][]string, path map[Point]bool) {
+	for i := 0; i < len(maze); i++ {
+		for j := 0; j < len(maze[i]); j++ {
+			if path[Point{i, j}] {
+				fmt.Print("O")
+			} else {
+				fmt.Print(maze[i][j])
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func clone_grid(grid [][]string) [][]string {
+	new_grid := [][]string{}
+	for i := 0; i < len(grid); i++ {
+		new_grid = append(new_grid, []string{})
+		for j := 0; j < len(grid[i]); j++ {
+			new_grid[i] = append(new_grid[i], grid[i][j])
+		}
+	}
+	return new_grid
 }
--- solutions/day_seventeen.go
+++ solutions/day_seventeen.go
@@ -82,7 +82,6 @@ func Day_seventeen_part_one() {
 			}
 			registers[2] = registers[0] / divisor
 		}
-		fmt.Println(instruction_set[i], instruction_set[i+1], registers, values)
 		i += 2
 	}
 	str_output := ""
@@ -103,8 +102,78 @@ func Day_seventeen_part_two() {
 		panic(err)
 	}
 	scanner := bufio.NewScanner(strings.NewReader(string(dat)))
+	registers := []int{}
+	instruction_set := []int{}
+	interpret_instructions := false
 	for scanner.Scan() {
+		if len(scanner.Text()) == 0 {
+			interpret_instructions = true
+			continue
+		}
+		if !interpret_instructions {
+			split_chars := strings.Split(scanner.Text(), ": ")
+			reg_val, _ := strconv.Atoi(split_chars[1])
+			registers = append(registers, reg_val)
+		} else {
+			split_chars := strings.Split(scanner.Text(), ": ")
+			for _, char := range strings.Split(split_chars[1], ",") {
+				instruction, _ := strconv.Atoi(char)
+				instruction_set = append(instruction_set, instruction)
+			}
+		}
 	}
+	i := 0
+	values := []int{}
+	for i < len(instruction_set)-1 {
+		switch instruction_set[i] {
+		case 0:
+			divisor := 1
+			combo := find_combo(instruction_set[i+1], registers)
+			for j := 0; j < combo; j++ {
+				divisor = divisor * 2
+			}
+			registers[0] = registers[0] / divisor
+		case 1:
+			registers[1] = registers[1] ^ instruction_set[i+1]
+		case 2:
+			combo := find_combo(instruction_set[i+1], registers)
+			registers[1] = combo & 7
+		case 3:
+			if registers[0] != 0 {
+				i = instruction_set[i+1]
+				continue
+			}
+		case 4:
+			registers[1] = registers[1] ^ registers[2]
+		case 5:
+			combo := find_combo(instruction_set[i+1], registers)
+			values = append(values, combo&7)
+		case 6:
+			divisor := 1
+			combo := find_combo(instruction_set[i+1], registers)
+			for j := 0; j < combo; j++ {
+				divisor = divisor * 2
+			}
+			registers[1] = registers[0] / divisor
+		case 7:
+			divisor := 1
+			combo := find_combo(instruction_set[i+1], registers)
+			for j := 0; j < combo; j++ {
+				divisor = divisor * 2
+			}
+			registers[2] = registers[0] / divisor
+		}
+		i += 2
+	}
+	str_output := ""
+	for i := 0; i < len(values); i++ {
+		str_output += strconv.Itoa(values[i])
+		if i != len(values)-1 {
+			str_output += ","
+		}
+	}
+	// fmt.Println(str_output)
+	// fmt.Println(values)
 }
 
 func find_combo(combo int, registers []int) int {
