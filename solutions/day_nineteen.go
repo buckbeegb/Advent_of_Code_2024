14cf112fc077388a63e95dc1968725532b201d41
Brendan Buckbee <buckbeegb@gmail.com>
2024-12-21 08:30:25 -0600

    Completed day 20 solution, will come back for day 19 later

--- solutions/day_nineteen.go
+++ solutions/day_nineteen.go
@@ -8,6 +8,13 @@ import (
 	"strings"
 )
 
+type towel_subset struct {
+	towel         string
+	valid_methods int
+	parents       []towel_subset
+	children      []towel_subset
+}
+
 func Day_nineteen_part_one() {
 	dat, err := os.ReadFile("./Full_Inputs/day_nineteen.txt")
 	// dat, err := os.ReadFile("./Test_Inputs/day_nineteen.txt")
@@ -15,8 +22,67 @@ func Day_nineteen_part_one() {
 		panic(err)
 	}
 	scanner := bufio.NewScanner(strings.NewReader(string(dat)))
+	valid_towels := make(map[string]bool)
+	towel_sets := []string{}
+	set_valid := true
+	max_len := 0
 	for scanner.Scan() {
+		if scanner.Text() == "" {
+			set_valid = false
+			continue
+		}
+		if set_valid {
+			for _, towel := range strings.Split(scanner.Text(), ", ") {
+				if len(towel) > max_len {
+					max_len = len(towel)
+				}
+				valid_towels[towel] = true
+			}
+		} else {
+			towel_sets = append(towel_sets, scanner.Text())
+		}
 	}
+	// towel_subsets := make(map[string]towel_subset)
+	// total_valid := 0
+	// for i := 0; i < len(towel_sets); i++ {
+	// 	_, found := towel_subsets[towel_sets[i]]
+	// 	if !found {
+	// 		towel_subsets[towel_sets[i]] = towel_subset{towel: towel_sets[i], valid_methods: 0}
+	// 	}
+	// 	search_strings := []string{towel_sets[i]}
+	// 	complete_match := false
+	// 	for len(search_strings) > 0 {
+	// 		addl_strings := []string{}
+	// 		for k := max_len; k > 0; k-- {
+	// 			if k > len(search_strings[0]) {
+	// 				continue
+	// 			}
+	// 			substring := search_strings[0][:k]
+	// 			subset, found := towel_subsets[substring]
+	// 			if found && subset.valid_methods > 0 {
+	// 				total_valid++
+	// 			}
+	// 			if valid_towels[search_strings[0][:k]] {
+	// 				if k == len(search_strings[0]) {
+	// 					towel_subsets[search_strings[0]] = towel_subset{towel: search_strings[0], valid_methods: 1}
+	// 				}
+	// 				addl_strings = append(addl_strings, search_strings[0][k:])
+	// 			}
+	// 			if complete_match {
+	// 				break
+	// 			}
+	// 		}
+	// 		if complete_match {
+	// 			break
+	// 		}
+	// 		search_strings = append(addl_strings, search_strings[1:]...)
+	// 	}
+	// 	if complete_match {
+	// 		fmt.Printf("Valid: %s\n", towel_sets[i])
+	// 		total_valid++
+	// 	}
+	// }
+	// fmt.Println(total_valid)
 }
 func Day_nineteen_part_two() {
 	dat, err := os.ReadFile("./Full_Inputs/day_nineteen.txt")
--- solutions/day_twenty.go
+++ solutions/day_twenty.go
@@ -2,11 +2,16 @@ package solutions
 
 import (
 	"bufio"
-	// "fmt"
+	"fmt"
 	"os"
 	// "strconv"
 	"strings"
 )
+
+type Pair struct {
+	start Point
+	end   Point
+}
 
 func Day_twenty_part_one() {
 	dat, err := os.ReadFile("./Full_Inputs/day_twenty.txt")
@@ -15,9 +20,49 @@ func Day_twenty_part_one() {
 		panic(err)
 	}
 	scanner := bufio.NewScanner(strings.NewReader(string(dat)))
+	maze := [][]string{}
+	start := Point{0, 0}
+	end := Point{0, 0}
 	for scanner.Scan() {
+		maze = append(maze, []string{})
+		for _, char := range scanner.Text() {
+			maze[len(maze)-1] = append(maze[len(maze)-1], string(char))
+			if char == 'S' {
+				start = Point{len(maze) - 1, len(maze[len(maze)-1]) - 1}
+			} else if char == 'E' {
+				end = Point{len(maze) - 1, len(maze[len(maze)-1]) - 1}
+			}
+		}
 	}
+	path := make(map[Point]int)
+	path[start] = 0
+	for start != end {
+		for _, dir := range []Point{{0, 1}, {0, -1}, {1, 0}, {-1, 0}} {
+			next := Point{start.row + dir.row, start.col + dir.col}
+			if path[next] == 0 && (maze[next.row][next.col] == "." || maze[next.row][next.col] == "E") {
+				path[next] = path[start] + 1
+				start = next
+			}
+			continue
+		}
+	}
+	cheats := make(map[int]int)
+	for key := range path {
+		for _, dir := range []Point{{0, 2}, {0, -2}, {2, 0}, {-2, 0}} {
+			next := Point{key.row + dir.row, key.col + dir.col}
+			if path[next] > (path[key] + 2) {
+				cheat_size := path[next] - path[key] - 2
+				if cheat_size >= 100 {
+					cheats[100]++
+				} else {
+					cheats[cheat_size]++
+				}
+			}
+		}
+	}
+	fmt.Println(cheats[100])
 }
+
 func Day_twenty_part_two() {
 	dat, err := os.ReadFile("./Full_Inputs/day_twenty.txt")
 	// dat, err := os.ReadFile("./Test_Inputs/day_twenty.txt")
@@ -25,6 +70,67 @@ func Day_twenty_part_two() {
 		panic(err)
 	}
 	scanner := bufio.NewScanner(strings.NewReader(string(dat)))
+	maze := [][]string{}
+	start := Point{0, 0}
+	end := Point{0, 0}
 	for scanner.Scan() {
+		maze = append(maze, []string{})
+		for _, char := range scanner.Text() {
+			maze[len(maze)-1] = append(maze[len(maze)-1], string(char))
+			if char == 'S' {
+				start = Point{len(maze) - 1, len(maze[len(maze)-1]) - 1}
+			} else if char == 'E' {
+				end = Point{len(maze) - 1, len(maze[len(maze)-1]) - 1}
+			}
+		}
 	}
+	path := make(map[Point]int)
+	path[start] = 0
+	for start != end {
+		for _, dir := range []Point{{0, 1}, {0, -1}, {1, 0}, {-1, 0}} {
+			next := Point{start.row + dir.row, start.col + dir.col}
+			if path[next] == 0 && (maze[next.row][next.col] == "." || maze[next.row][next.col] == "E") {
+				path[next] = path[start] + 1
+				start = next
+			}
+			continue
+		}
+	}
+	cheats := make(map[int]int)
+	cheats_list := make(map[Pair]bool)
+	for key := range path {
+		for i := 0; i < 21; i++ {
+			for j := 0; j < 21; j++ {
+				for _, dir := range []Point{{i, j}, {-i, j}, {-i, -j}, {i, -j}} {
+					next := Point{key.row + dir.row, key.col + dir.col}
+					row_dist := 0
+					if dir.row > 0 {
+						row_dist = next.row - key.row
+					} else {
+						row_dist = key.row - next.row
+					}
+					col_dist := 0
+					if dir.col > 0 {
+						col_dist = next.col - key.col
+					} else {
+						col_dist = key.col - next.col
+					}
+					if row_dist+col_dist > 20 {
+						continue
+					}
+					if path[next] > (path[key]+(row_dist+col_dist+49)) && !cheats_list[Pair{key, next}] {
+						cheat_size := path[next] - path[key] - (row_dist + col_dist)
+						cheats_list[Pair{key, next}] = true
+						if cheat_size >= 100 {
+							cheats[100]++
+						} else {
+							cheats[cheat_size]++
+						}
+					}
+				}
+			}
+		}
+	}
+	fmt.Println(cheats[100])
+	// fmt.Println(cheats)
 }
