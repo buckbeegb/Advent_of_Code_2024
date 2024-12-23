4212e71ef434805fcdd472f32fd35aa1848adee0
Brendan Buckbee <buckbeegb@gmail.com>
2024-12-23 16:09:25 -0600

    Optimized Day 20 Solution :)

--- solutions/day_twenty.go
+++ solutions/day_twenty.go
@@ -8,10 +8,8 @@ import (
 	"strings"
 )
 
-type Pair struct {
-	start Point
-	end   Point
-}
+var path map[Point]int
+var path_list []Point
 
 func Day_twenty_part_one() {
 	dat, err := os.ReadFile("./Full_Inputs/day_twenty.txt")
@@ -34,102 +32,61 @@ func Day_twenty_part_one() {
 			}
 		}
 	}
-	path := make(map[Point]int)
+	path = make(map[Point]int)
+	path_list = []Point{start}
 	path[start] = 0
 	for start != end {
 		for _, dir := range []Point{{0, 1}, {0, -1}, {1, 0}, {-1, 0}} {
 			next := Point{start.row + dir.row, start.col + dir.col}
-			if path[next] == 0 && (maze[next.row][next.col] == "." || maze[next.row][next.col] == "E") {
+			if maze[next.row][next.col] == "." || maze[next.row][next.col] == "E" {
+				maze[next.row][next.col] = "O"
 				path[next] = path[start] + 1
+				path_list = append(path_list, next)
 				start = next
+				break
 			}
-			continue
 		}
 	}
-	cheats := make(map[int]int)
+	over_100_cheats := 0
 	for key := range path {
 		for _, dir := range []Point{{0, 2}, {0, -2}, {2, 0}, {-2, 0}} {
 			next := Point{key.row + dir.row, key.col + dir.col}
 			if path[next] > (path[key] + 2) {
 				cheat_size := path[next] - path[key] - 2
 				if cheat_size >= 100 {
-					cheats[100]++
-				} else {
-					cheats[cheat_size]++
+					over_100_cheats++
 				}
 			}
 		}
 	}
-	fmt.Println(cheats[100])
+	fmt.Println(over_100_cheats)
 }
 
 func Day_twenty_part_two() {
-	dat, err := os.ReadFile("./Full_Inputs/day_twenty.txt")
-	// dat, err := os.ReadFile("./Test_Inputs/day_twenty.txt")
-	if err != nil {
-		panic(err)
-	}
-	scanner := bufio.NewScanner(strings.NewReader(string(dat)))
-	maze := [][]string{}
-	start := Point{0, 0}
-	end := Point{0, 0}
-	for scanner.Scan() {
-		maze = append(maze, []string{})
-		for _, char := range scanner.Text() {
-			maze[len(maze)-1] = append(maze[len(maze)-1], string(char))
-			if char == 'S' {
-				start = Point{len(maze) - 1, len(maze[len(maze)-1]) - 1}
-			} else if char == 'E' {
-				end = Point{len(maze) - 1, len(maze[len(maze)-1]) - 1}
+	over_100_cheats := 0
+	for i := 0; i < len(path_list)-100; i++ {
+		for j := i + 100; j < len(path_list); j++ {
+			dist := get_distance_between_points(path_list[i], path_list[j])
+			if dist > 20 {
+				j += (dist - 21)
+				continue
+			}
+			if j-i-dist >= 100 {
+				over_100_cheats++
 			}
 		}
 	}
-	path := make(map[Point]int)
-	path[start] = 0
-	for start != end {
-		for _, dir := range []Point{{0, 1}, {0, -1}, {1, 0}, {-1, 0}} {
-			next := Point{start.row + dir.row, start.col + dir.col}
-			if path[next] == 0 && (maze[next.row][next.col] == "." || maze[next.row][next.col] == "E") {
-				path[next] = path[start] + 1
-				start = next
-			}
-			continue
-		}
+	fmt.Println(over_100_cheats)
+}
+
+func get_distance_between_points(start Point, end Point) int {
+	row_dist := start.row - end.row
+	col_dist := start.col - end.col
+	if row_dist < 0 {
+		row_dist = -row_dist
 	}
-	cheats := make(map[int]int)
-	cheats_list := make(map[Pair]bool)
-	for key := range path {
-		for i := 0; i < 21; i++ {
-			for j := 0; j < 21; j++ {
-				for _, dir := range []Point{{i, j}, {-i, j}, {-i, -j}, {i, -j}} {
-					next := Point{key.row + dir.row, key.col + dir.col}
-					row_dist := 0
-					if dir.row > 0 {
-						row_dist = next.row - key.row
-					} else {
-						row_dist = key.row - next.row
-					}
-					col_dist := 0
-					if dir.col > 0 {
-						col_dist = next.col - key.col
-					} else {
-						col_dist = key.col - next.col
-					}
-					if row_dist+col_dist > 20 {
-						continue
-					}
-					if path[next] > (path[key]+(row_dist+col_dist+49)) && !cheats_list[Pair{key, next}] {
-						cheat_size := path[next] - path[key] - (row_dist + col_dist)
-						cheats_list[Pair{key, next}] = true
-						if cheat_size >= 100 {
-							cheats[100]++
-						} else {
-							cheats[cheat_size]++
-						}
-					}
-				}
-			}
-		}
+	if col_dist < 0 {
+		col_dist = -col_dist
 	}
-	fmt.Println(cheats[100])
+	return row_dist + col_dist
 }
